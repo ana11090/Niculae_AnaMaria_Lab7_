6c7a96bfb4b8c6c877a0aecc42e3307d8413c837
ana11090 <72621705+ana11090@users.noreply.github.com>
2024-01-07 19:09:27 +0200

    Inainte de sarcina

---
+++ LibraryModel/Data/LibraryContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryModel.Data
+{
+    public class LibraryContextFactory : IDesignTimeDbContextFactory<LibraryContext>
+    {
+        public LibraryContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LibraryContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LibraryNew;TrustServerCertificate=True;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new LibraryContext(optionsBuilder.Options);
+        }
+    }
+
+}
